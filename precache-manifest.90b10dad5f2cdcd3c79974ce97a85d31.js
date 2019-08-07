570d4f1bef641ef4b0c40eabcc107751ea5bc166
SMLM <farsight@hotmail.com>
2019-08-07 00:07:32 -0700

    Updates

---
+++ precache-manifest.90b10dad5f2cdcd3c79974ce97a85d31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e781389c2f7d207b395cc0c5a0dce894",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "283fd83d30ad64ceb7c4",
+    "url": "/Clicky-Game/static/css/main.3e2f00b7.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "283fd83d30ad64ceb7c4",
+    "url": "/Clicky-Game/static/js/main.c7e8490c.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
